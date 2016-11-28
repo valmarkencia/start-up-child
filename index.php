cabb9ed2bcbe11a9449706bbde99045cff7da195
mark anthony valencia <valmarkencia@gmail.com>
2016-11-28 16:05:44 +0800

    google analytics setup

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 <head>
 	<title>Graphic Design — Mark Anthony Valencia</title>
 	<link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+
 	<script>
 	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
@@ -13,8 +16,5 @@
 	  ga('send', 'pageview');
 
 	</script>
-</head>
-<body>
-
 </body>
 </html>
